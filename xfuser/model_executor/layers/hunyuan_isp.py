c4e0fd935553d8d832ee6934f7689df539483288
FrankLeeeee <somerlee.9@gmail.com>
2025-02-07 07:39:30 +0000

    supported isp for hunyuan transformer single block

--- xfuser/model_executor/layers/hunyuan_isp.py
+++ xfuser/model_executor/layers/hunyuan_isp.py
@@ -10,6 +10,7 @@ import torch.distributed as dist
 import torch.nn.functional as F
 
 
+COMMUNICATION_STREAM = torch.cuda.Stream()
 
 def all_gather(x, gather_dim, process_group=None, no_cat: bool = False, stream=None):
     if stream is not None:
@@ -237,11 +238,11 @@ def modify_transformer_block(block: HunyuanVideoTransformerBlock):
                 merged_chunk = torch.cat(chunk_list, dim=1).contiguous()
             else:
                 merged_chunk = chunk_list[0].contiguous()
-            self.communication_stream.wait_stream(torch.cuda.current_stream())
-            output_list.extend(all_gather(merged_chunk, gather_dim=1, no_cat=True, stream=self.communication_stream))
+            COMMUNICATION_STREAM.wait_stream(torch.cuda.current_stream())
+            output_list.extend(all_gather(merged_chunk, gather_dim=1, no_cat=True, stream=COMMUNICATION_STREAM))
 
         
-        torch.cuda.current_stream().wait_stream(self.communication_stream)
+        torch.cuda.current_stream().wait_stream(COMMUNICATION_STREAM)
 
         # order the output list
         # the output list is in list [1, 2, 3, 4, 1, 2, 3, 4, ...], order it to be [1, 1, 2, 2, 3, 3, 4, 4, ...]
@@ -260,7 +261,6 @@ def modify_transformer_block(block: HunyuanVideoTransformerBlock):
         return hidden_states, encoder_hidden_states
 
     new_forward = new_forward.__get__(block)
-    block.communication_stream = torch.cuda.Stream()
     block.forward = new_forward
 
 
@@ -276,33 +276,170 @@ def modify_single_transformer_block(block: HunyuanVideoSingleTransformerBlock):
         attention_mask: Optional[torch.Tensor] = None,
         image_rotary_emb: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
     ) -> torch.Tensor:
+        # ========================
+        # some global variables
+        ws = dist.get_world_size()
+        rank = dist.get_rank()
+        global_hidden_states_len = hidden_states.shape[1]
+        global_encoder_hidden_states_len = encoder_hidden_states.shape[1]
+        sub_seq_len_per_device = (global_hidden_states_len + global_encoder_hidden_states_len) // ws
+        global_sequence_start = rank * sub_seq_len_per_device
+        global_sequence_end = global_sequence_start + sub_seq_len_per_device
+        assert sub_seq_len_per_device * ws == global_hidden_states_len + global_encoder_hidden_states_len, \
+            f"The combined sequence length {global_hidden_states_len + global_encoder_hidden_states_len} is not divisible by the world size {ws}"
+
         text_seq_length = encoder_hidden_states.shape[1]
+
         hidden_states = torch.cat([hidden_states, encoder_hidden_states], dim=1)
+        residual = hidden_states.chunk(ws, dim=1)[rank].contiguous()
 
-        residual = hidden_states
-
+        # ========================
         # 1. Input normalization
+        # ========================
         norm_hidden_states, gate = self.norm(hidden_states, emb=temb)
-        mlp_hidden_states = self.act_mlp(self.proj_mlp(norm_hidden_states))
+        sub_norm_hidden_states = norm_hidden_states.chunk(ws, dim=1)[rank].contiguous()
+        sub_mlp_hidden_states = self.act_mlp(self.proj_mlp(sub_norm_hidden_states))
 
-        norm_hidden_states, norm_encoder_hidden_states = (
-            norm_hidden_states[:, :-text_seq_length, :],
-            norm_hidden_states[:, -text_seq_length:, :],
-        )
-
+        # ========================
         # 2. Attention
-        attn_output, context_attn_output = self.attn(
-            hidden_states=norm_hidden_states,
-            encoder_hidden_states=norm_encoder_hidden_states,
-            attention_mask=attention_mask,
-            image_rotary_emb=image_rotary_emb,
-        )
-        attn_output = torch.cat([attn_output, context_attn_output], dim=1)
+        # ========================
+        # norm_hidden_states, norm_encoder_hidden_states = (
+        #     norm_hidden_states[:, :-text_seq_length, :],
+        #     norm_hidden_states[:, -text_seq_length:, :],
+        # )
+        # attn_output, context_attn_output = self.attn(
+        #     hidden_states=norm_hidden_states,
+        #     encoder_hidden_states=norm_encoder_hidden_states,
+        #     attention_mask=attention_mask,
+        #     image_rotary_emb=image_rotary_emb,
+        # )
 
-        # 3. Modulation and residual connection
-        hidden_states = torch.cat([attn_output, mlp_hidden_states], dim=2)
-        hidden_states = gate.unsqueeze(1) * self.proj_out(hidden_states)
-        hidden_states = hidden_states + residual
+        # simply the code by removing some if-else branches
+        # so that we make assertion here to make sure the inputs are consistent
+        assert self.attn.add_q_proj is None and encoder_hidden_states is not None
+        
+
+        # 1. QKV projections
+        query = self.attn.to_q(sub_norm_hidden_states)
+        key = self.attn.to_k(norm_hidden_states)
+        value = self.attn.to_v(norm_hidden_states)
+
+        query = query.unflatten(2, (self.attn.heads, -1)).transpose(1, 2)
+        key = key.unflatten(2, (self.attn.heads, -1)).transpose(1, 2)
+        value = value.unflatten(2, (self.attn.heads, -1)).transpose(1, 2)
+
+        # 2. QK normalization
+        if self.attn.norm_q is not None:
+            query = self.attn.norm_q(query)
+        if self.attn.norm_k is not None:
+            key = self.attn.norm_k(key)
+
+        # 3. Rotational positional embeddings applied to latent stream
+        if image_rotary_emb is not None:
+            from diffusers.models.embeddings import apply_rotary_emb
+            # because query is a partition of the whole sequence, we need to apply the rotary
+            # embedding on different segments
+            if global_sequence_end < global_hidden_states_len:
+                # case 1: query contains only hidden states
+                sub_image_rotary_emb = [
+                    val.narrow(dim=0, start=global_sequence_start, length=global_sequence_end - global_sequence_start).contiguous()
+                    for val in image_rotary_emb
+                ]
+                query = apply_rotary_emb(query, sub_image_rotary_emb)
+            elif global_sequence_start >= global_hidden_states_len:
+                # case 2: query contains only encoder hidden states
+                query = query
+            else:
+                # case 3: query contains both hidden states and encoder hidden states
+                sub_image_rotary_emb = [
+                    val.narrow(
+                        dim=0, 
+                        start=global_sequence_start,
+                        length=global_hidden_states_len - global_sequence_start
+                    ).contiguous()
+                    for val in image_rotary_emb
+                ]
+                encoder_segment_len = global_sequence_end - global_hidden_states_len
+                query = torch.cat(
+                    [
+                        apply_rotary_emb(query[:, :, : -encoder_segment_len], sub_image_rotary_emb),
+                        query[:, :, -encoder_segment_len :],
+                    ],
+                    dim=2,
+                )
+
+            # we can keep key the same
+            key = torch.cat(
+                [
+                    apply_rotary_emb(key[:, :, : -encoder_hidden_states.shape[1]], image_rotary_emb),
+                    key[:, :, -encoder_hidden_states.shape[1] :],
+                ],
+                dim=2,
+            )
+
+        # 5. Attention
+        output_list = []
+        for chunk_idx, (sub_query_chunk, sub_mlp_chunk, sub_residual) in enumerate(zip(query.chunk(ws, dim=2), sub_mlp_hidden_states.chunk(ws, dim=1), residual.chunk(ws, dim=1))):
+            # compute attention
+            global_chunk_start = global_sequence_start + chunk_idx * sub_seq_len_per_device // ws
+            chunk_size = sub_query_chunk.shape[2]
+            sub_attention_mask = attention_mask.narrow(dim=1, start=global_chunk_start, length=chunk_size).contiguous()
+            sub_attn_output = F.scaled_dot_product_attention(
+                sub_query_chunk, key, value, attn_mask=sub_attention_mask, dropout_p=0.0, is_causal=False
+            )
+            sub_attn_output = sub_attn_output.transpose(1, 2).flatten(2, 3)
+            sub_attn_output = sub_attn_output.to(query.dtype)
+
+            # 6. Output projection
+            to_be_merged = []
+            if encoder_hidden_states is not None:
+                if global_sequence_end < global_hidden_states_len:
+                    # case 1: attn_output contains only hidden states
+                    sub_attn_output, sub_encoder_hidden_states = sub_attn_output, None
+                elif global_sequence_start >= global_hidden_states_len:
+                    # case 2: attn_output contains only encoder hidden states
+                    sub_attn_output, sub_encoder_hidden_states = None, sub_attn_output
+                else:
+                    # case 3: attn_output contains both hidden states and encoder hidden states
+                    encoder_segment_len = global_sequence_end - global_hidden_states_len
+                    sub_attn_output, sub_encoder_hidden_states = (
+                        sub_attn_output[:, :-encoder_segment_len],
+                        sub_attn_output[:, -encoder_segment_len :],
+                    )
+
+                if getattr(self.attn, "to_out", None) is not None and sub_attn_output is not None:
+                    sub_attn_output = self.attn.to_out[0](sub_attn_output)
+                    sub_attn_output = self.attn.to_out[1](sub_attn_output)
+                    
+                if getattr(self.attn, "to_add_out", None) is not None and sub_encoder_hidden_states is not None:
+                    sub_encoder_hidden_states = self.attn.to_add_out(sub_encoder_hidden_states)
+
+                if sub_attn_output is not None:
+                    to_be_merged.append(sub_attn_output)
+                if sub_encoder_hidden_states is not None:
+                    to_be_merged.append(sub_encoder_hidden_states)
+            sub_attn_output = torch.cat(to_be_merged, dim=1)
+
+            # ========================
+            # 3. Modulation and residual connection
+            # ========================
+            sub_hidden_states = torch.cat([sub_attn_output, sub_mlp_chunk], dim=2)
+            sub_hidden_states = gate.unsqueeze(1) * self.proj_out(sub_hidden_states)
+            sub_hidden_states = sub_hidden_states + sub_residual
+
+            # ISP: run all gather here
+            COMMUNICATION_STREAM.wait_stream(torch.cuda.current_stream())
+            output_list.extend(all_gather(sub_hidden_states, gather_dim=1, no_cat=True, stream=COMMUNICATION_STREAM))
+        
+        torch.cuda.current_stream().wait_stream(COMMUNICATION_STREAM)
+
+        # order the output list
+        # the output list is in list [1, 2, 3, 4, 1, 2, 3, 4, ...], order it to be [1, 1, 2, 2, 3, 3, 4, 4, ...]
+        sorted_output_list = []
+        for i in range(ws):
+            for j in range(i, len(output_list), ws):
+                sorted_output_list.append(output_list[j])
+        hidden_states = torch.cat(sorted_output_list, dim=1)
 
         hidden_states, encoder_hidden_states = (
             hidden_states[:, :-text_seq_length, :],
